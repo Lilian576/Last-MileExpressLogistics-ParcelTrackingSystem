8bab03c74bd8c21377451ab0911cfee2f2429e84
Nhuquynh8406 <nhuquynhle804@gmail.com>
2026-08-17 23:46:00 +0700

    feat: implement pricing engine with Haversine distance calculation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParcelsModule } from './parcels/parcels.module';
+import { AuthModule } from './auth/auth.module';
+import { TrackingModule } from './tracking/tracking.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ParcelsModule, AuthModule, TrackingModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
