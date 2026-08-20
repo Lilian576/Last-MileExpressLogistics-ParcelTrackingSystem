0884e1757b435911f409855dc46164f568ea2ad8
Nhuquynh8406 <nhuquynhle804@gmail.com>
2026-08-20 18:42:25 +0700

    feat: add WebSocket gateway with ping-pong test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ParcelsModule } from './parcels/parcels.module';
+import { TrackingModule } from './tracking/tracking.module';
 
 @Module({
-  imports: [ParcelsModule],
+  imports: [ParcelsModule, TrackingModule],
   controllers: [AppController],
   providers: [AppService],
 })
